2b882cfb5506d24291a1d313b1a7e8829b72590c
linyuxiaoran <420343280@qq.com>
2017-08-30 11:08:00 +0800

    保存爬取回来的报文

---
+++ tool/SavaData1.py
@@ -0,0 +1,18 @@
+'''
+Created on 2017年8月30日
+保存爬取回来的报文
+@author: Lin Yu
+'''
+def saveFile(data):
+    save_path = 'D:\DataSave\temp.out'
+    f_obj = open(save_path, 'wb') #wb 表示打开方式
+    f_obj.write(data)
+    f_obj.close()
+
+data = '' #此代码为防止编译器报错，使用时请删去此行。
+#这里省略爬虫代码
+#......
+
+#爬到的数据保存在 data变量里
+#将data变量保存到D盘下
+saveFile(data)
